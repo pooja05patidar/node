1ffca53e2f0eae52004a9b1c35cb6dbcc2086f21
Pooja <poojamiss511patidar@gmail.com>
2025-02-06 11:46:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const fs = require('fs')
+console.log('Pooja Patidar')
+fs.writeFileSync("hello.txt", "fs is non global module")
---
+++ simple.js
@@ -0,0 +1,11 @@
+const app = require('./app')
+var a = 10;
+var b = 20;
+console.warn(a+b)
+console.log(app.y)
+arr = [3, 5, 6]
+i = arr.filter((item) => {
+    return item===3
+    // console.log(item);
+})
+console.log(i)
